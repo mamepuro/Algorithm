739db09a7fbc7826b6c67b6c5a342d3fabd57c8e
mamepuro <beans.mamegame.laugh@icloud.com>
2019-07-19 18:11:20 +0900

    挿入ソートを追加

--- sort/bubble-sort.cpp
+++ sort/bubble-sort.cpp
@@ -1,4 +1,4 @@
-/*バブルソート 
+/*バブルソート(bubble sort)
 　隣り合う数値を比較してソートを行う。計算量はO(n^2)
 　ゲームへの応用例：少人数戦闘を行うRPGでの攻撃順位の決定
  */
@@ -11,7 +11,7 @@ int main(){
     int data[n];//各データの数値
     int flag[n];//ソート済みの判定フラグ変数
     int relay;//数値入れ換えの中継をする変数
-    int m =n;
+    //int m =n;
     for(int i=0;i<n;i++){
         
         cin >> data[i];
@@ -19,7 +19,7 @@ int main(){
     }
     //ソートする
     for (int j=0;j<n;j++){
-        for(int i=1;i<m;i++){
+        for(int i=1;i<n;i++){
 
             if(data[i-1]>=data[i]){
                 //if(flag[i]==1){
